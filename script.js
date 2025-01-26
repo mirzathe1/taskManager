02cbd3ec72265105b34671303ea3663f2bb8a449
Mirza Mehedi Iqbal <mirza.theone@gmail.com>
2025-01-26 12:19:39 +0600

    added delete button to listItem

--- script.js
+++ script.js
@@ -1,23 +1,41 @@
 // Grab the necessary DOM elements
 const taskinput = document.getElementById('taskInput');
 const prioritySelect = document.getElementById('prioritySelect');
-const addTaskButton =document.getElementById('addTaskButton');
-const taskList =document.getElementById('taskList');
+const addTaskButton = document.getElementById('addTaskButton');
+const taskList = document.getElementById('taskList');
 
 // add an eventListener to AddTask Button
 
-addTaskButton.addEventListener('click',()=>{
+addTaskButton.addEventListener('click', () => {
     // Get the values from the input box and dropdown
     const taskText = taskinput.value.trim();
     const taskPriority = prioritySelect.value;
-// validate Input
-    if(!taskText){
+    // validate Input
+    if (!taskText) {
         alert("please enter a text");
         return;
     }
-// create a new list
-    const listItem= document.createElement('li');
-    listItem.textContent =taskText;
+    // create a new list
+    const listItem = document.createElement('li');
+    listItem.textContent = taskText;
     taskList.appendChild(listItem);
-    taskinput.value="";
+    taskinput.value = "";
+
+    // create delete button
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.style.marginLeft = '10px';
+    listItem.append(deleteButton);
+
+
+    deleteButton.addEventListener('click', () => {
+        listItem.remove();
+        // taskList.removeChild(listItem);
+    })
+    
 })
+
+
+
+
+
